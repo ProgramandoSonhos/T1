a07d0f573b8d5600ea674b8f9b94fc61fc6da841
Cecilia Ribeiro <cecilia.rb@gmail.com>
2019-11-13 23:47:46 -0300

    Create index.php

---
+++ aula1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Alo mundo!</title>
+		<script type="text/javascript">
+			alert('oi!')
+		</script>
+	</head>
+
+	<body>
+		<h1>Hello world!</h1>
+		<h2>Bom dia, bem vindas ao curso</h2>
+		<?php
+			echo "Hello world!";
+		?>
+	</body>
+</html>
